ad2d8798e70f2bac530542cd61eff6b50cc6369d
laylaradwan <layla.radwan@hotmail.fr>
2024-01-15 23:46:44 +0100

    Mise à jour de app.py

--- app.py
+++ app.py
@@ -1,6 +1,1 @@
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    print(f"Starting server on port {port}")
-    print("Hello, World!")
+print("Hello, World!")
